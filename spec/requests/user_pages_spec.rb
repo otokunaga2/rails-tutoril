68c7194c966a61c2ddfb7cecc8a516980d31d295
Seiki Tokunaga <otokuna46@gmail.com>
2015-04-20 05:15:19 +0900

    helper

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+#RSpec.describe "UserPages", type: :request do
+describe "User pages" do
+  subject { page }
+  describe "signup page" do
+    before { visit signup }
+    it {should have_content('Sign up')}
+    it {should have_title('Sign up')}
+  end
+  #describe "GET /user_pages" do
+  #  it "works! (now write some real specs)" do
+  #    get user_pages_index_path
+  #    expect(response).to have_http_status(200)
+  #  end
+  #end
+end
