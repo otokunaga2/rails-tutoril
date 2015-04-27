8669dba8c795131db3af32309001b82197c855b0
Seiki Tokunaga <otokuna46@gmail.com>
2015-04-27 23:34:45 +0900

    start sec6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,28 @@
 require 'spec_helper'
 describe "Static pages" do
   subject { page }
+
+  shared_examples_for "all static pages" do
+    it { should have_content(heading)}
+    it { should have_title(full_title(page_title))}
+  end
+
   describe "Home page" do
     before {visit root_path}
-    it { should have_content('Sample App')}
-    it { should have_title(full_title(''))}
-    it { should_not have_title('| Home')}
+    let(:heading) {'Sample App'}
+    let(:page_title){''}
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('|Home')}
+  end
+  describe "Help page" do
+
+  end
+  describe "About page" do
+
+  end
+  describe "Conent page" do
+
   end
 end
 
