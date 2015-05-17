3afb4af9abddb1cc25371034caad3ae6319fca75
Seiki Tokunaga <otokuna46@gmail.com>
2015-05-17 21:29:47 +0900

    add _header

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  #与えられたユーザーのGravatar (http://gravatar.com/) を返す。
+  def gravatar_for(user)
+    gravater_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravater_url = "https://secure.gravatar.com/avatar/#{gravater_id}"
+    image_tag(gravater_url, alt: user.name, class: "gravatar")
+  end
 end
