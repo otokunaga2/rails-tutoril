ffdfaad13afcb54660b7e136674af27e2d270f1c
Seiki Tokunaga <otokuna46@gmail.com>
2015-05-17 23:37:10 +0900

    sec7 途中まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,17 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
