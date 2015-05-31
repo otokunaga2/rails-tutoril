36cdb60a52e4344dfe8eda1127bab2bce9e68751
Seiki Tokunaga <otokuna46@gmail.com>
2015-05-31 22:36:45 +0900

    Add SSL in production

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,7 @@
 module UsersHelper
-  #与えられたユーザーのGravatar (http://gravatar.com/) を返す。
   def gravatar_for(user)
-    gravater_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravater_url = "https://secure.gravatar.com/avatar/#{gravater_id}"
-    image_tag(gravater_url, alt: user.name, class: "gravatar")
+gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -12,10 +12,25 @@ describe "User page" do
     it { should have_title(user.name) }
   end
 
-  describe "signup page" do
+  describe "signup" do
     before { visit signup_path}
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
+    let(:submit) { "Create my account" }
+    describe "invalid click account" do
+      it "should not a create user" do
+        expect { click_button "Create my account" }.not_to change(User, :count)
+      end
+    end
+    describe "with valid information" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "user@example.com"
+        fill_in "Password",     with: "foobar"
+        fill_in "Confirmation", with: "foobar"
+      end
+      it "should create a user" do
+        expect{click_button submit}.to change(User, :count).by(1)
+      end
+    end
     #let(:submit){"Create My Account"}
     #describe "with invalid information" do
     #  it "should not create a user" do
